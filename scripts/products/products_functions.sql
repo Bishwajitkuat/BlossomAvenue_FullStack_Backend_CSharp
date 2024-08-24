677b129ada8af7e166a991e8e5a13ffab8d3452a
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-08-24 22:15:21 +0300

    Improve get_product_by_id plpgsql func
    So that it will return average product review ration when it is called.

--- scripts/products/products_functions.sql
+++ scripts/products/products_functions.sql
@@ -82,7 +82,8 @@ RETURNS TABLE
   title VARCHAR, 
   description TEXT, 
   min_price DECIMAL, 
-  image_url TEXT
+  image_url TEXT,
+  avg_star DECIMAL
   )
 AS
 $$
@@ -90,9 +91,16 @@ BEGIN
 RETURN QUERY
 WITH sub AS
   (
-    SELECT v.product_id, MIN(v.price) AS min_p, MIN(i.image_url) AS image_url FROM variations v
+    SELECT 
+    v.product_id, 
+    MIN(v.price) AS min_p, 
+    MIN(i.image_url) AS image_url,
+    AVG(r.star) AS star 
+    FROM variations v
     JOIN images i
     ON v.product_id = i.product_id
+    LEFT JOIN product_reviews r
+    ON v.product_id = r.product_id
     WHERE v.product_id = id
     GROUP BY v.product_id
   )
@@ -101,7 +109,8 @@ SELECT
   p.title, 
   p.description, 
   s.min_p, 
-  s.image_url 
+  s.image_url,
+  CAST(s.star AS DECIMAL(10,1)) 
   FROM products p
   JOIN sub s
   ON p.product_id = s.product_id;
