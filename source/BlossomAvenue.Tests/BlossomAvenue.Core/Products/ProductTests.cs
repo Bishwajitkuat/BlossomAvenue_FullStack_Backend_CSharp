c4369e52e27748000f03222cdbf89c96482d2d7e
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-08-26 22:56:02 +0300

    Add tests for Product class

---
+++ source/BlossomAvenue.Tests/BlossomAvenue.Core/Products/ProductTests.cs
@@ -0,0 +1,62 @@
+using BlossomAvenue.Core.ProductAggregate;
+
+namespace BlossomAvenue.Tests.BlossomAvenue.Core.Products
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void ProductClass_ShouldExists()
+        {
+            // Act
+            var classType = Type.GetType("BlossomAvenue.Core.ProductAggregate.Product, BlossomAvenue.Core");
+
+            // Assert
+            Assert.NotNull(classType);
+        }
+
+        [Fact]
+        public void Product_ShouldHaveValidProductId()
+        {
+            //Arrange
+            var product = typeof(Product);
+
+            // Act
+            var productId = product.GetProperty("ProductId");
+
+            // Assert
+            Assert.NotNull(productId);
+            Assert.Equal(typeof(Guid), productId.PropertyType);
+        }
+
+        [Fact]
+        public void Product_ShouldHaveValidTitle()
+        {
+            //Arrange
+            var product = typeof(Product);
+
+            // Act
+            var title = product.GetProperty("Title");
+
+
+            // Assert
+            Assert.NotNull(title);
+            Assert.Equal(typeof(string), title.PropertyType);
+
+
+        }
+
+        [Fact]
+        public void Product_ShouldHaveValidDescription()
+        {
+            //Arrange
+            var product = typeof(Product);
+
+            // Act
+            var description = product.GetProperty("Description");
+
+            // Assert
+            Assert.NotNull(description);
+            Assert.Equal(typeof(string), description.PropertyType);
+        }
+    }
+}
