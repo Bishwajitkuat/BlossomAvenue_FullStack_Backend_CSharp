8c4cd81991bdef7c5408e305cd45e3ca19070887
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-08-28 11:37:53 +0300

    Add CreateProductDto class

---
+++ source/BlossomAvenue.Service/ProductsServices/ProductDto.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlossomAvenue.Core.ProductReviews;
+using BlossomAvenue.Core.Products;
+
+namespace BlossomAvenue.Service.ProductsServices
+{
+    public class CreateProductDto
+
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<CreateImageDto> Images { get; set; }
+        public IEnumerable<CreateVariationDto> Variations { get; set; }
+        public IEnumerable<CreateProductCategoryDto> ProductCategories { get; set; }
+
+        public Product ConvertToProduct()
+        {
+            Guid productId = Guid.NewGuid();
+            IEnumerable<Image> images = Images.Select(i => i.ConvertToImage(productId));
+            IEnumerable<Variation> variations = Variations.Select(v => v.ConvertToVariation(productId));
+            IEnumerable<ProductCategory> productCategories = ProductCategories.Select(pc => pc.ConvertToProductCategory(productId));
+
+            return new Product
+            {
+                ProductId = productId,
+                Title = this.Title,
+                Description = this.Description,
+                Images = images,
+                Variations = variations,
+                ProductCategories = productCategories,
+            };
+
+
+        }
+    }
+
+}
