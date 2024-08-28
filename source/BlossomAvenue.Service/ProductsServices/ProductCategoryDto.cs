3f5ec0db8ecf1be1e7e9de5ce59c08a392f3e3fd
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-08-28 11:36:56 +0300

    Add CreateProductCategoryDto class

---
+++ source/BlossomAvenue.Service/ProductsServices/ProductCategoryDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlossomAvenue.Core.Products;
+
+namespace BlossomAvenue.Service.ProductsServices
+{
+    public class CreateProductCategoryDto
+    {
+        public Guid CategoryId { get; set; }
+
+        public ProductCategory ConvertToProductCategory(Guid productId)
+        {
+            return new ProductCategory
+            {
+                ProductCategoryId = Guid.NewGuid(),
+                CategoryId = this.CategoryId,
+                ProductId = productId,
+            };
+        }
+
+    }
+}
