bca4dd14bd261acc31bdcd3909ed2550c57c6c38
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-06 14:53:16 +0300

    Use HashPassword correctly And passing hass passowed and salt value to UserCredential object.

--- source/BlossomAvenue.Service/UsersService/UserManagement.cs
+++ source/BlossomAvenue.Service/UsersService/UserManagement.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using BlossomAvenue.Service.Repositories.Cities;
 using BlossomAvenue.Service.Cryptography;
 using BlossomAvenue.Service.UsersService.Dtos;
+using System.Security.Cryptography;
 
 namespace BlossomAvenue.Service.UsersService
 {
@@ -74,93 +75,95 @@ namespace BlossomAvenue.Service.UsersService
             return _mapper.Map<UserDto>(createdUser);
         }
 
-            public async Task<CreateDetailedUserResponseDto> CreateProfile(CreateDetailedUserDto profile)
+        public async Task<CreateDetailedUserResponseDto> CreateProfile(CreateDetailedUserDto profile)
+        {
+            if (await _userRepository.CheckUserExistsByEmail(profile.Email!)) throw new RecordAlreadyExistsException(typeof(User).Name);
+
+            if (!(await _cityRepository.IsCityExists(profile.CityId))) throw new RecordNotFoundException(typeof(City).Name);
+
+            var userRole = await GetUserRole();
+
+            var userEntity = _mapper.Map<User>(profile);
+
+            userEntity.UserRole = userRole;
+            _passwordHasher.HashPassword(profile.Password, out string hashedPassword, out byte[] salt);
+
+            userEntity.UserCredential = new UserCredential()
             {
-                if (await _userRepository.CheckUserExistsByEmail(profile.Email!)) throw new RecordAlreadyExistsException(typeof(User).Name);
+                UserName = profile.UserName,
+                Password = hashedPassword,
+                Salt = salt
+            };
 
-                if (!(await _cityRepository.IsCityExists(profile.CityId))) throw new RecordNotFoundException(typeof(City).Name);
+            var savedUser = await _userRepository.CreateUser(userEntity);
 
-                var userRole = await GetUserRole();
+            var returnProfile = _mapper.Map<CreateDetailedUserResponseDto>(profile);
+            returnProfile.UserId = savedUser.UserId;
+            returnProfile.Password = String.Empty;
 
-                var userEntity = _mapper.Map<User>(profile);
+            return returnProfile;
+        }
 
-                userEntity.UserRole = userRole;
+        public void DeleteUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
 
-                userEntity.UserCredential = new UserCredential() 
-                {
-                    UserName = profile.UserName,
-                    Password = _passwordHasher.HashPassword(profile.Password)
-                };
+        public async Task<UserDetailedDto> GetUser(Guid userId)
+        {
+            var user = await _userRepository.GetUser(userId);
+            return user is null ? throw new RecordNotFoundException("User") : _mapper.Map<UserDetailedDto>(user);
+        }
 
-                var savedUser = await _userRepository.CreateUser(userEntity);
+        public async Task<List<UserDto>> GetUsers(UsersQueryDto query)
+        {
+            var users = await _userRepository.GetUsers(query);
+            return _mapper.Map<List<UserDto>>(users);
+        }
 
-                var returnProfile = _mapper.Map<CreateDetailedUserResponseDto>(profile);
-                returnProfile.UserId = savedUser.UserId;
-                returnProfile.Password = String.Empty;
+        public async Task UpdateUser(Guid userId, CreateUpdateUserDto user)
+        {
+            var existing = await _userRepository.GetUser(userId) ?? throw new RecordNotFoundException(typeof(User).Name);
 
-                return returnProfile;
-            }
+            if (await _userRepository.CheckEmailExistsWithOtherUsers(userId, user.Email!)) throw new RecordAlreadyExistsException("Email");
 
-            public void DeleteUser(Guid userId)
-            {
-                throw new NotImplementedException();
-            }
+            existing.FirstName = user.FirstName;
+            existing.LastName = user.LastName;
+            existing.Email = user.Email;
 
-            public async Task<UserDetailedDto> GetUser(Guid userId)
-            {
-                var user = await _userRepository.GetUser(userId);
-                return user is null ? throw new RecordNotFoundException("User") : _mapper.Map<UserDetailedDto>(user);
-            }
-
-            public async Task<List<UserDto>> GetUsers(UsersQueryDto query)
-            {
-                var users = await _userRepository.GetUsers(query);
-                return _mapper.Map<List<UserDto>>(users);
-            }
-
-            public async Task UpdateUser(Guid userId, CreateUpdateUserDto user) 
-            {
-                var existing = await _userRepository.GetUser(userId) ?? throw new RecordNotFoundException(typeof(User).Name);
-            
-                if (await _userRepository.CheckEmailExistsWithOtherUsers(userId, user.Email!)) throw new RecordAlreadyExistsException("Email");
-
-                existing.FirstName = user.FirstName;
-                existing.LastName = user.LastName;
-                existing.Email = user.Email;
-                
-                await _userRepository.UpdateUser(existing);
-            }
+            await _userRepository.UpdateUser(existing);
+        }
 
         private async Task<UserRole> GetAdminRole()
+        {
+            var adminRoleName = _configuration.GetSection("UserRoles").GetSection("Admin").Value;
+
+            if (string.IsNullOrEmpty(adminRoleName))
             {
-                var adminRoleName = _configuration.GetSection("UserRoles").GetSection("Admin").Value;
-
-                if (string.IsNullOrEmpty(adminRoleName))
-                {
-                    throw new RecordNotFoundException("Admin Role in config");
-                }
-
-                return await GetRole(adminRoleName);
+                throw new RecordNotFoundException("Admin Role in config");
             }
 
-            private async Task<UserRole> GetUserRole()
-            {
-                var adminRoleName = _configuration.GetSection("UserRoles").GetSection("User").Value;
-                if (string.IsNullOrEmpty(adminRoleName))
-                {
-                    throw new RecordNotFoundException("User Role in config");
-                }
+            return await GetRole(adminRoleName);
+        }
 
-                return await GetRole(adminRoleName);
+        private async Task<UserRole> GetUserRole()
+        {
+            var adminRoleName = _configuration.GetSection("UserRoles").GetSection("User").Value;
+            if (string.IsNullOrEmpty(adminRoleName))
+            {
+                throw new RecordNotFoundException("User Role in config");
             }
 
-            private async Task<UserRole> GetRole(string roleName)
-            {
-                var userRole = await _userRoleRepository.GetUserRoleByName(roleName);
-                return userRole is null ? throw new RecordNotFoundException("User Role") : userRole;
-            }
+            return await GetRole(adminRoleName);
+        }
 
-        
+        private async Task<UserRole> GetRole(string roleName)
+        {
+            var userRole = await _userRoleRepository.GetUserRoleByName(roleName);
+            return userRole is null ? throw new RecordNotFoundException("User Role") : userRole;
+        }
+
+
     }
-    }
+}
 
