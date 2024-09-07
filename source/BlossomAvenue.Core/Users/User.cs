b17286d6332d58222ef34cc5f6a309177262f2d3
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-07 12:41:04 +0300

    Add UserRole enum and removed UserRoleId

--- source/BlossomAvenue.Core/Users/User.cs
+++ source/BlossomAvenue.Core/Users/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using BlossomAvenue.Core.ValueTypes;
 
 namespace BlossomAvenue.Core.Users;
 
@@ -13,19 +14,19 @@ public partial class User
 
     public string? Email { get; set; }
 
-    public Guid UserRoleId { get; set; }
+    public virtual BlossomAvenue.Core.ValueTypes.UserRole UserRole { get; set; } = BlossomAvenue.Core.ValueTypes.UserRole.Customer;
+    // public Guid UserRoleId { get; set; }
 
     public DateTime? LastLogin { get; set; }
 
     public bool? IsUserActive { get; set; }
 
     public DateTime? CreatedAt { get; set; }
+    public virtual UserCredential UserCredential { get; set; } = null!;
+    public virtual ICollection<UserContactNumber> UserContactNumbers { get; set; } = new List<UserContactNumber>();
 
     public virtual ICollection<UserAddress> UserAddresses { get; set; } = new List<UserAddress>();
 
-    public virtual UserRole UserRole { get; set; } = null!;
 
-    public virtual ICollection<UserContactNumber> UserContactNumbers { get; set; } = new List<UserContactNumber>();
 
-    public virtual UserCredential UserCredential { get; set; } = null!;
 }
