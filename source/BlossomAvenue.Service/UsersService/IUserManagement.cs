03edc8b74e9b7b0fc7404b0a0d79d49ad9d72f77
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-08 00:35:24 +0300

    Remove ActiveInactiveUser, CreateUser mthods Admin can change user active status and role by UpdateUser method, so ActiveInactiveUser method is redundent. Admin user will not create any profile, rather change the user role, so CreateUser is unnecessary.

--- source/BlossomAvenue.Service/UsersService/IUserManagement.cs
+++ source/BlossomAvenue.Service/UsersService/IUserManagement.cs
@@ -1,4 +1,4 @@
-using BlossomAvenue.Core.Users;
+﻿using BlossomAvenue.Core.Users;
 using BlossomAvenue.Service.UsersService.Dtos;
 using System;
 using System.Collections.Generic;
@@ -10,13 +10,11 @@ namespace BlossomAvenue.Service.UsersService
 {
     public interface IUserManagement
     {
-        public Task<UserDto> CreateUser(Dtos.CreateUpdateUserDto user);
         public Task<List<UserDto>> GetUsers(UsersQueryDto query);
         public Task<User> GetUser(Guid userId);
         public Task<bool> UpdateUser(Guid userId, UpdateUserDto updateUserDto);
         public Task<bool> UpdateUserProfile(UpdateDetailedUserDto updateDetailedUserDto);
         public void DeleteUser(Guid userId);
-        public Task ActiveInactiveUser(Guid userId, bool status);
         public Task<User> CreateProfile(User profile);
     }
 }
