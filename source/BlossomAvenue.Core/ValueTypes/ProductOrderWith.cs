d4ffd4637437301b7d6dec7e5261f58a28c4ee08
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-08 23:37:37 +0300

    Add possible product order with as enum.

---
+++ source/BlossomAvenue.Core/ValueTypes/ProductOrderWith.cs
@@ -0,0 +1,11 @@
+namespace BlossomAvenue.Core.ValueTypes
+{
+    public enum ProductOrderWith
+    {
+        Price = 1,
+        Inventory = 2,
+        Title = 3,
+        CreatedAt = 4,
+
+    }
+}
--- source/BlossomAvenue.Service/ProductsServices/ProductQueryDto.cs
+++ source/BlossomAvenue.Service/ProductsServices/ProductQueryDto.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlossomAvenue.Core.ValueTypes;
 using BlossomAvenue.Service.SharedDtos;
 
 namespace BlossomAvenue.Service.ProductsServices
 {
     public class ProductQueryDto : SharedPagination
     {
+        public ProductOrderWith? ProductOrderWith { get; set; }
 
     }
 }
