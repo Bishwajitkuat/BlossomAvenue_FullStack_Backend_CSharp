7ffed43f197ceae6c6477cbb067ab484e903fc48
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-10 13:20:44 +0300

    Improve CreateCartItemsDto

--- source/BlossomAvenue.Service/CartsService/CartItemsDto.cs
+++ source/BlossomAvenue.Service/CartsService/CartItemsDto.cs
@@ -8,14 +8,18 @@ namespace BlossomAvenue.Service.CartsService
 {
     public class CreateCartItemsDto
     {
-        public Guid CartId { get; set; }
         public Guid ProductId { get; set; }
         public Guid VariationId { get; set; }
         public int Quantity { get; set; }
 
-        public CartItem ConvertToCartitems()
+        public CartItem ConvertToCartItems()
         {
-            return new CartItem { CartItemsId = Guid.NewGuid(), CartId = this.CartId, ProductId = this.ProductId, Quantity = this.Quantity, VariationId = this.VariationId };
+            return new CartItem
+            {
+                ProductId = ProductId,
+                Quantity = Quantity,
+                VariationId = VariationId
+            };
         }
 
     }
