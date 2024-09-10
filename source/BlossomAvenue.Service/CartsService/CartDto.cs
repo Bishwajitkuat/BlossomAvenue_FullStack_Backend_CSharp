fb339445aaaf1bc01a78d31ead62773b178125c9
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-10 13:22:11 +0300

    Add ReadCartDto class.

--- source/BlossomAvenue.Service/CartsService/CartDto.cs
+++ source/BlossomAvenue.Service/CartsService/CartDto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlossomAvenue.Core.Carts;
 
 namespace BlossomAvenue.Service.CartsService
 {
@@ -10,4 +11,23 @@ namespace BlossomAvenue.Service.CartsService
         public Guid Id { get; set; }
         public List<CartItemDto> CartItems { get; set; }
     }
+
+    public class ReadCartDto
+    {
+        public Guid CartId { get; set; }
+
+        public virtual ICollection<ReadCartItemDto> CartItems { get; set; }
+
+        public ReadCartDto(Cart cart)
+        {
+            CartId = cart.CartId;
+            CartItems = cart.CartItems.Select(ci => new ReadCartItemDto(ci)).ToList();
+        }
+
+
+    }
+
+
+
+
 }
