a81baa1b8a192a2afdfd7dfe9f4a81576901d815
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-11 03:07:47 +0300

    Add query object for sorting and filtering orders.

---
+++ source/BlossomAvenue.Core/ValueTypes/OrderOrderWith.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlossomAvenue.Core.ValueTypes
+{
+    public enum OrderOrderWith
+    {
+        CreatedAt = 1,
+        TotalAmount = 2,
+    }
+}
