9a5d57220049d57c4494ddebb92f49ccfefbd796
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-13 04:34:17 +0300

    Modify Login method If user is authenticated with username and password, a jwt token and a refresh token will be generated. refresh token will saved in database. returns both tokens into the controller layer.

--- source/BlossomAvenue.Service/AuthenticationService/AuthManagement.cs
+++ source/BlossomAvenue.Service/AuthenticationService/AuthManagement.cs
@@ -1,11 +1,14 @@
-﻿using BlossomAvenue.Core.Products;
+﻿using BlossomAvenue.Core.Authentication;
+using BlossomAvenue.Core.Products;
 using BlossomAvenue.Service.Cryptography;
 using BlossomAvenue.Service.CustomExceptions;
+using BlossomAvenue.Service.Repositories.Authentications;
 using BlossomAvenue.Service.Repositories.InMemory;
 using BlossomAvenue.Service.Repositories.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,15 +16,17 @@ namespace BlossomAvenue.Service.AuthenticationService
 {
     public class AuthManagement : IAuthManagement
     {
-        private readonly IUserRepository _userRepository;
+        private IUserRepository _userRepository;
+        private IRefreshTokenRepository _refreshTokenRepository;
         private readonly ITokenManagement _tokenMgt;
         private readonly IPasswordHasher _passwordHasher;
 
-        public AuthManagement(IUserRepository userRepository, ITokenManagement tokenMgt, IPasswordHasher passwordHasher)
+        public AuthManagement(IUserRepository userRepository, ITokenManagement tokenMgt, IPasswordHasher passwordHasher, IRefreshTokenRepository refreshTokenRepository)
         {
             _userRepository = userRepository;
             _tokenMgt = tokenMgt;
             _passwordHasher = passwordHasher;
+            _refreshTokenRepository = refreshTokenRepository;
         }
         public async Task<AuthenticationResultDto> Login(string username, string password)
         {
@@ -29,17 +34,29 @@ namespace BlossomAvenue.Service.AuthenticationService
 
             if (user == null)
             {
-                return new AuthenticationResultDto() { IsAuthenticated = false };
-
+                return new AuthenticationResultDto { LoginResponseDto = new LoginResponseDto() { IsAuthenticated = false }, RefreshToken = null };
             }
 
             if (_passwordHasher.VerifyPassword(password, user.UserCredential.Password, user.UserCredential.Salt))
             {
                 var token = _tokenMgt.GenerateToken(user);
-                return new AuthenticationResultDto { IsAuthenticated = true, Token = token };
+                // generate refresh token
+                var refreshToken = _tokenMgt.GenerateRefreshToken(user);
+                // save refresh token in db
+                var isSaved = await _refreshTokenRepository.AddRefreshToken(refreshToken);
+                if (isSaved)
+                {
+                    return new AuthenticationResultDto
+                    {
+                        LoginResponseDto = new LoginResponseDto { IsAuthenticated = true, UserRole = user.UserRole, Token = token },
+                        RefreshToken = refreshToken
+                    };
+                }
+                else throw new Exception("Failed to save the refresh token in the db");
             }
-            return new AuthenticationResultDto() { IsAuthenticated = false };
+            return new AuthenticationResultDto { LoginResponseDto = new LoginResponseDto() { IsAuthenticated = false }, RefreshToken = null };
         }
+
 
         public void Logout(string token)
         {
