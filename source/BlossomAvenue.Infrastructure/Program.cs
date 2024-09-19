516123d3245d6881014bd4bc926d29108d96d399
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-09-19 03:05:03 +0300

    Allow swagger to be availabe at production.

--- source/BlossomAvenue.Infrastructure/Program.cs
+++ source/BlossomAvenue.Infrastructure/Program.cs
@@ -157,16 +157,15 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
-    app.UseDeveloperExceptionPage();
-}
+app.UseDeveloperExceptionPage();
+
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseHttpsRedirection();
